d68ac064ef495425746b773e4855f5fa44e3fa4c
Kostia Chaikovskyi <chaichka175@gmail.com>
2021-08-27 11:49:13 +0300

    test

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Profile } from "./components/Profile/Profile";
+import user from "./components/data/profile.json";
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </div>
+  );
+};
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,47 @@
+import PropTypes from "prop-types";
+import s from "./Profile.module.css";
+export const Profile = ({
+  name,
+  tag,
+  location,
+  avatar,
+  likes,
+  followers,
+  views,
+}) => {
+  return (
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img src={avatar} alt="Аватар пользователя" className={s.avatar} />
+        <p className={s.name}>{name}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
+
+      <ul className={s.sadadstats}>
+        <li>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  likes: PropTypes.number.isRequired,
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+};
